4e098bc98d0a3c66795516abf41985629f2a8525
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2022-12-15 00:17:21 +0530

    Index Page

---
+++ d_home.php
@@ -0,0 +1,17 @@
+<?php
+    include('d_lock.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor Home</title>
+</head>
+<body>
+    <center><h2>Doctor Home</h2></center>
+    <button><a href="d_logout.php">Logout</a></button>
+</body>
+</html>
