dcd2995c3eed7d5966765227aba4fd5c8b68e64d
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2022-12-16 01:13:50 +0530

    patient form

---
+++ p_home.php
@@ -0,0 +1,17 @@
+<?php
+    include('p_lock.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient Home</title>
+</head>
+<body>
+    <center><h2>Patient Home</h2></center>
+    <button><a href="p_logout.php">Logout</a></button>
+</body>
+</html>
